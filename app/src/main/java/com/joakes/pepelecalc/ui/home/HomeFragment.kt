b084dec8680251f8087080616aed43db1c1ad4fd
justin <justin@justinoak.es>
2025-07-25 21:41:02 -0500

    UI Experiments

--- app/src/main/java/com/joakes/pepelecalc/ui/home/HomeFragment.kt
+++ app/src/main/java/com/joakes/pepelecalc/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.joakes.pepelecalc.databinding.FragmentCalculateBinding
 
 class HomeFragment : Fragment() {
@@ -22,8 +20,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentCalculateBinding.inflate(inflater, container, false)
         val root: View = binding.root
