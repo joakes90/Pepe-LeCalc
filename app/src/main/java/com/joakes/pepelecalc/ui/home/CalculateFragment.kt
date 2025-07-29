50a4d4d33114a008dc12c52df44497e71ce1fe29
justin <justin@justinoak.es>
2025-07-29 17:13:27 -0500

    Configuring syringe ticks

--- app/src/main/java/com/joakes/pepelecalc/ui/home/CalculateFragment.kt
+++ app/src/main/java/com/joakes/pepelecalc/ui/home/CalculateFragment.kt
@@ -1,5 +1,7 @@
 package com.joakes.pepelecalc.ui.home
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,12 +9,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.joakes.pepelecalc.R
 import com.joakes.pepelecalc.databinding.FragmentCalculateBinding
 
 class CalculateFragment : Fragment() {
 
+    private companion object {
+        const val TICK_WIDTH = 3 // 2dp
+        const val TICK_HEIGHT_MAJOR = 60 // 30dp
+        const val TICK_HEIGHT_MINOR = 30 // 15dp
+    }
     private var _binding: FragmentCalculateBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,7 +37,23 @@ class CalculateFragment : Fragment() {
         _binding = FragmentCalculateBinding.inflate(inflater, container, false)
 
         val root: View = binding.root
+        val tickMarksContainer = binding.tickMarksContainer
 
+        // Syringe Volume Selection
+        val syringeVolumeGroup = binding.syringeVolumeGroup
+        syringeVolumeGroup.setOnCheckedChangeListener { _, checkedId ->
+            val syringeVolume = when (checkedId) {
+                R.id.volume30 -> calculateViewModel.syringeVolume.value = 30
+                R.id.volume50 -> calculateViewModel.syringeVolume.value = 50
+                R.id.volume100 -> calculateViewModel.syringeVolume.value = 100
+                // Add more cases for other volume options
+                else -> {
+                    calculateViewModel.syringeVolume.value = 100
+                }
+            }
+            drawTickMarks(tickMarksContainer, calculateViewModel.syringeVolume.value ?: 100)
+        }
+        // Peptide Weight Input
         val weightEditText: EditText = binding.peptideWeight
         val textWatcher = object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -55,5 +80,48 @@ class CalculateFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun drawTickMarks(tickMarksContainer: LinearLayout, maxUnits: Int) {
+        tickMarksContainer.removeAllViews()
+        val majorInterval = 5
+        val minorInterval = 1
+
+        tickMarksContainer.post {
+            val containerWidth = tickMarksContainer.width
+            val totalTicks = maxUnits + 1
+
+            val totalTickWidth = totalTicks * TICK_WIDTH
+            val availableSpace = containerWidth - totalTickWidth
+            val spaceBetweenTicks = availableSpace / maxUnits.toFloat()
+
+            for (unit in 0..maxUnits) {
+                val isMajor = unit % majorInterval == 0
+                val tickMark = createTickMark(
+                    isMajor,
+                    spaceBetweenTicks,
+                    isFirst = unit == 0
+                )
+                tickMarksContainer.addView(tickMark)
+            }
+        }
+    }
+
+    private fun createTickMark(
+        isMajor: Boolean,
+        spaceBetweenTicks: Float,
+        isFirst: Boolean
+    ): View {
+        return View(requireContext()).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                TICK_WIDTH,
+                if (isMajor) TICK_HEIGHT_MAJOR else TICK_HEIGHT_MINOR
+            ).apply {
+                if (!isFirst) {
+                    marginStart = spaceBetweenTicks.toInt()
+                }
+            }
+            setBackgroundColor(if (isMajor) Color.BLACK else Color.GRAY)
+        }
     }
 }
--- app/src/main/java/com/joakes/pepelecalc/ui/home/CalculateViewModel.kt
+++ app/src/main/java/com/joakes/pepelecalc/ui/home/CalculateViewModel.kt
@@ -8,6 +8,10 @@ class CalculateViewModel : ViewModel() {
     private val _weight = MutableLiveData<Int?>().apply {
         value = null
     }
+    private val _syringeVolume = MutableLiveData<Int>().apply {
+        value = 30
+    }
 
     var weight: MutableLiveData<Int?> = _weight
+    var syringeVolume: MutableLiveData<Int> = _syringeVolume
 }
